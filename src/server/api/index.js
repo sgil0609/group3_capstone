f0c59c1de631464d637c758c44a6add14984053c
System Administrator <root@Kellys-MBP.hsd1.co.comcast.net>
2024-04-18 19:15:50 -0600

    API get all products

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,42 +1,9 @@
+// server/api/index.js
 const express = require('express');
-const apiRouter = express.Router();
-const jwt = require('jsonwebtoken');
+const productsRouter = require('../routes/product');
 
-const volleyball = require('volleyball')
-apiRouter.use(volleyball)
+const router = express.Router();
 
-// TO BE COMPLETED - set `req.user` if possible, using token sent in the request header
-apiRouter.use(async (req, res, next) => {
-  const auth = req.header('Authorization');
-  
-  if (!auth) { 
-    next();
-  } 
-  else if (auth.startsWith('REPLACE_ME')) {
-    // TODO - Get JUST the token out of 'auth'
-    const token = 'REPLACE_ME';
-    
-    try {
-      const parsedToken = 'REPLACE_ME';
-      // TODO - Call 'jwt.verify()' to see if the token is valid. If it is, use it to get the user's 'id'. Look up the user with their 'id' and set 'req.user'
+router.use('/products', productsRouter);
 
-    } catch (error) {
-      next(error);
-    }
-  } 
-  else {
-    next({
-      name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with 'Bearer'`
-    });
-  }
-});
-
-const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
-
-apiRouter.use((err, req, res, next) => {
-    res.status(500).send(err)
-  })
-
-module.exports = apiRouter;
+module.exports = router;
