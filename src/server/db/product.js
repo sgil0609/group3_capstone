05a38dfbffdccd975db7c434e639e4c0adca6ed8
DCy4 <davidacalle@gmail.com>
2024-04-18 21:46:36 -0400

    refactored api, routes folder

---
+++ src/server/db/product.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function getProductId() {
+  const product = await prisma.product.findFirst();
+  return product.id;
+}
+
+async function createProduct(name, description) {
+  const product = await prisma.product.create({
+    data: {
+      name: name,
+      description: description,
+    },
+  });
+  return product;
+}
+
+async function deleteProduct(id) {
+  const product = await prisma.product.delete({
+    where: {
+      id: id,
+    },
+  });
+  return product;
+}
+
+module.exports = {
+  getProductId,
+  createProduct,
+  deleteProduct,
+};
--- src/server/routes/product.js
+++ src/server/routes/product.js
@@ -1,17 +1,9 @@
-const express = require("express");
+const express = require('express');
+const { getProducts, createProduct } = require('../api/product');
+
 const router = express.Router();
 
-// Product routes
-router.get("/products", (req, res) => {
-  res.json({ message: "product retrieved successfully" });
-});
-
-router.get("/products/:id", (req, res) => {});
-
-router.post("/products", (req, res) => {});
-
-router.put("/products/:id", (req, res) => {});
-
-router.delete("/products/:id", (req, res) => {});
+router.get('/', getProducts);
+router.post('/product/id:', create )
 
 module.exports = router;
