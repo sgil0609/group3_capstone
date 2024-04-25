7b74082fb369274ae97bea2827d5dc8b59af4e64
Shaun <shaun@Kellys-MBP.hsd1.co.comcast.net>
2024-04-25 17:59:50 -0600

    added category API

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -5,10 +5,13 @@ const router = express.Router();
 const productRouter = require("./product");
 const usersRouter = require("./users");
 const authRouter = require("./auth");
+const productCategory = require("./category");
+
 
 router.use("/product", productRouter);
 router.use("/users", usersRouter);
 router.use("/auth", authRouter);
+router.use("/category", productCategory);
 // cart router
 
 module.exports = router;
