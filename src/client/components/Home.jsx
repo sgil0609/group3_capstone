8864da9eefcbd675ebe67aacf80c4391b639bf4c
annieha04 <itsannha@gmail.com>
2024-04-25 21:29:58 -0400

    edit photo

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -4,7 +4,6 @@ import SummerImage from "../assets/images/A-Summer.png";
 export default function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
             <div className="SummerImage">
                 <img src={SummerImage} alt="sumr"/>
                 </div>
