47ddec6f93879a140f0d7a3b60c7f1c285a16e42
annieha04 <itsannha@gmail.com>
2024-04-25 20:37:28 -0400

    added register component

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SummerImage from "../assets/images/A-Summer.png";
 
 export default function Home() {
     return (
         <div className="home">
             <h1>Home</h1>
+            <div className="SummerImage">
+                <img src={SummerImage} alt="sumr"/>
+                </div>
         </div>
     );
 }
---
+++ src/client/components/Register.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [dob, setDob] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleDobChange = (e) => {
+    setDob(e.target.value);
+  };
+
+  const register = async() => {
+    try {
+        const response = await fetch('http://localhost:3000/api/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            }, 
+            body: JSON.stringify({
+                email,
+                password,
+                dob
+            })
+        });
+        const result = await response.json();
+        setMessage(result.message);
+        if(!response.ok) {
+          throw(result)
+        }
+        setEmail('');
+        setPassword('');
+        setDob('');
+    } catch (err) {
+        console.error(`${err.name}: ${err.message}`);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    register();
+  };
+
+  return (
+    <div className="register">
+      <h2>Become a SADD member</h2>
+      <h4>Become a Member - you'll enjoy exclusive deals, offers, invites, and rewards.</h4>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Create a password:</label>
+          <input
+            type='password'
+            id='password'
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='dob'>Date of Birth:</label>
+          <input
+            type='date'
+            id='dob'
+            value={dob}
+            onChange={handleDobChange}
+            required
+          />
+        </div>
+        <button type='submit'>Register</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Register;
