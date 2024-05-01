5b28c6c8003f2e0ba46e5e435bf702b85a0e614f
Adade1188 <derrickyad@hotmail.com>
2024-05-01 18:41:28 -0400

    modified Productdetails m

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -8,79 +8,116 @@ import Register from "./components/Register";
 import ProductList from "./components/ProductList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import './style.css';
-
-const SubNavHome = () => {
-  return (
-    <div className="subnav">
-      <Link className="subnavbtn" to="/">Home</Link>
-    </div>
-  );
-};
-
-const SubNavMens = ({ setSelectedCategory }) => {
-  return (
-    <div className="subnav">
-        <Link className="subnavbtn" to="/mens" onClick={() => setSelectedCategory("mens")}>Mens <i className="fa fa-caret-down"></i></Link>
-        <div className="subnav-content">
-            <Link to="/mens/tops">Tops</Link>
-            <Link to="/mens/bottoms">Bottoms</Link>
-            <Link to="/mens/shoes">Shoes</Link>
-        </div>
-    </div>
-  );
-};
-
-const SubNavWomens = ({ setSelectedCategory }) => {
-  return (
-    <div className="subnav">
-        <Link className="subnavbtn" to="/womens" onClick={() => setSelectedCategory("womens")}>Womens <i className="fa fa-caret-down"></i></Link>
-        <div className="subnav-content">
-            <Link to="/womens/tops">Tops</Link>
-            <Link to="/womens/bottoms">Bottoms</Link>
-            <Link to="/womens/shoes">Shoes</Link>
-        </div>
-    </div>
-  );
-};
+import "./style.css";
+import ProductDetails from "./components/ProductDetails";
 
 const SubNavAccount = () => {
   return (
     <div className="subnav">
-        <Link className="subnavbtn" to="/login">My Account <i className="fa fa-caret-down"></i></Link>
+      <Link className="subnavbtn" to="/login">
+        My Account <i className="fa fa-caret-down"></i>
+      </Link>
+      <div className="subnav-content">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </div>
+  );
+};
+
+const SubNavHome = () => {
+  return (
+    <div className="subnav">
+      <Link className="subnavbtn" to="/">
+        Home
+      </Link>
+    </div>
+  );
+};
+
+const SubNavMens = () => {
+  const setSelectedCategory = (category) => {};
+
+  return (
+    <div className="subnav">
+      <Link
+        className="subnavbtn"
+        to="/mens"
+        onClick={() => setSelectedCategory("mens")}
+      >
+        Mens <i className="fa fa-caret-down"></i>
+      </Link>
+      <div className="subnav-content">
+        <Link to="/mens/tops">Tops</Link>
+        <Link to="/mens/bottoms">Bottoms</Link>
+        <Link to="/mens/shoes">Shoes</Link>
+      </div>
+    </div>
+  );
+};
+
+const SubNavWomens = () => {
+  const setSelectedCategory = (category) => {
+    return (
+      <div className="subnav">
+        <Link
+          className="subnavbtn"
+          to="/womens"
+          onClick={() => setSelectedCategory("womens")}
+        >
+          Womens <i className="fa fa-caret-down"></i>
+        </Link>
         <div className="subnav-content">
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+          <Link to="/womens/tops">Tops</Link>
+          <Link to="/womens/bottoms">Bottoms</Link>
+          <Link to="/womens/shoes">Shoes</Link>
         </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="subnav">
+      <Link className="subnavbtn" to="/login">
+        My Account <i className="fa fa-caret-down"></i>
+      </Link>
+      <div className="subnav-content">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
     </div>
   );
 };
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   return (
-      <div>
-        <header>
-          <div className="logo">SADD</div>
-          <div className="navbar">
-            <SubNavHome />
-            <SubNavMens onClick = {() => setSelectedCategory("mens")} />
-            <SubNavWomens onClick = {() => setSelectedCategory("womens")}/>
-            <SubNavAccount />
-            <Link className="subnavbtn" to="/cart">
-              <FontAwesomeIcon icon={faShoppingCart} />
-            </Link>
-          </div>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/productlist" element={<ProductList category={selectedCategory}/>} />
-          <Route path="/mens" element={<ProductList category="mens"/>} />
-          <Route path="/womens" element={<ProductList category="womens"/>} />
-        </Routes>
+    <div>
+      <header>
+        <div className="logo">SADD</div>
+        <div className="navbar">
+          <SubNavHome />
+          <SubNavMens onClick={() => setSelectedCategory("mens")} />
+          <SubNavWomens onClick={() => setSelectedCategory("womens")} />
+          <SubNavAccount />
+          <Link className="subnavbtn" to="/cart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </Link>
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/productlist"
+          element={<ProductList category={selectedCategory} />}
+        />
+        <Route path="/mens" element={<ProductList category="mens" />} />
+        <Route path="/womens" element={<ProductList category="womens" />} />
+        <Route path="/product/:id/details" element={<ProductDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/client/components/ProductDetails.jsx
+++ src/client/components/ProductDetails.jsx
@@ -1,35 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { getProductById } from '../../server/api/product'; 
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 const ProductDetails = () => {
-    const { productId } = useParams();
-    const [product, setProduct] = useState(null);
+  const { id } = useParams(); // Use destructuring to get 'id' from URL params
+  const [product, setProduct] = useState(null);
 
-    useEffect(() => {
-        fetchProductDetails();
-    }, [productId]);
-
+  useEffect(() => {
     const fetchProductDetails = async () => {
-        try {
-            const productData = await getProductById(productId); 
-            setProduct(productData);
-        } catch (error) {
-            console.error(error);
+      try {
+        const response = await fetch(`http://localhost:3000/api/product/${id}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch product details");
         }
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error(error);
+      }
     };
+    fetchProductDetails();
+  }, [id]); // Fetch details whenever 'id' changes
 
-    if (!product) {
-        return <div>Loading...</div>;
-    }
+  if (!product) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div className="product-details">
-            <img src={product.image} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
-        </div>
-    );
+  return (
+    <div className="product-details">
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
 };
 
 export default ProductDetails;
