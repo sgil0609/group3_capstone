3006f3f3ab998a28381bb1f6d6b5c464f77b5ee9
annieha04 <itsannha@gmail.com>
2024-05-01 19:39:07 -0400

    added navbar component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,93 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import { SubNavHome, SubNavMens, SubNavWomens, SubNavAccount } from "./components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
-import ProductDetails from "./components/ProductDetails";
 
-const SubNavAccount = () => {
-  return (
-    <div className="subnav">
-      <Link className="subnavbtn" to="/login">
-        My Account <i className="fa fa-caret-down"></i>
-      </Link>
-      <div className="subnav-content">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
-    </div>
-  );
-};
-
-const SubNavHome = () => {
-  return (
-    <div className="subnav">
-      <Link className="subnavbtn" to="/">
-        Home
-      </Link>
-    </div>
-  );
-};
-
-const SubNavMens = () => {
-  const setSelectedCategory = (category) => {};
-
-  return (
-    <div className="subnav">
-      <Link
-        className="subnavbtn"
-        to="/mens"
-        onClick={() => setSelectedCategory("mens")}
-      >
-        Mens <i className="fa fa-caret-down"></i>
-      </Link>
-      <div className="subnav-content">
-        <Link to="/mens/tops">Tops</Link>
-        <Link to="/mens/bottoms">Bottoms</Link>
-        <Link to="/mens/shoes">Shoes</Link>
-      </div>
-    </div>
-  );
-};
-
-const SubNavWomens = () => {
-  const setSelectedCategory = (category) => {
-    return (
-      <div className="subnav">
-        <Link
-          className="subnavbtn"
-          to="/womens"
-          onClick={() => setSelectedCategory("womens")}
-        >
-          Womens <i className="fa fa-caret-down"></i>
-        </Link>
-        <div className="subnav-content">
-          <Link to="/womens/tops">Tops</Link>
-          <Link to="/womens/bottoms">Bottoms</Link>
-          <Link to="/womens/shoes">Shoes</Link>
-        </div>
-      </div>
-    );
-  };
-
-  return (
-    <div className="subnav">
-      <Link className="subnavbtn" to="/login">
-        My Account <i className="fa fa-caret-down"></i>
-      </Link>
-      <div className="subnav-content">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
-    </div>
-  );
-};
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -110,10 +33,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/productlist"
-          element={<ProductList category={selectedCategory} />}
-        />
+        <Route path="/productlist" element={<ProductList category={selectedCategory} />} />
         <Route path="/mens" element={<ProductList category="mens" />} />
         <Route path="/womens" element={<ProductList category="womens" />} />
         <Route path="/product/:id/details" element={<ProductDetails />} />
