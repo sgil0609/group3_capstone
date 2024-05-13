0804de91ebf2b8168d9a396fad2461635acce63b
Adade1188 <derrickyad@hotmail.com>
2024-05-13 19:50:24 -0400

    modified cart and app

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -1,19 +1,43 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Cart = ({ cartItems, setCartItems }) => {
   const calculateTotalCost = () => {
+    if (cartItems.length === 0) {
+      return 0;
+    }
+
     let total = 0;
     cartItems.forEach((item) => {
-      total += Number(item.price);
+      const itemPrice = Number(item.price);
+      const itemQuantity = Number(item.quantity);
+      if (!isNaN(itemPrice) && !isNaN(itemQuantity)) {
+        total += itemPrice * itemQuantity;
+      }
     });
     return total;
+  };
+
+  const handleRemoveItem = (id) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartItems);
+  };
+
+  const handleQuantityChange = (id, newQuantity) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
   };
 
   const handleCheckout = () => {
     setCartItems([]);
     alert("Checkout completed!");
   };
-  console.log(cartItems);
 
   return (
     <div>
@@ -25,7 +49,28 @@ const Cart = ({ cartItems, setCartItems }) => {
           <ul>
             {cartItems.map((item) => (
               <li key={item.id}>
-                {item.name} - ${item.price}
+                <div className="cart-item">
+                  
+                  <img src={item.imageUrl} alt="item.name" />
+
+                  <div>
+                    <p>Price: ${item.price}</p>
+                    <label htmlFor={`quantity-${item.id}`}>Qty:</label>
+                    <input
+                      type="number"
+                      id={`quantity-${item.id}`}
+                      value={item.quantity}
+                      onChange={(e) =>
+                        handleQuantityChange(item.id, e.target.value)
+                      }
+                    />
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      onClick={() => handleRemoveItem(item.id)}
+                      className="remove-icon"
+                    />
+                  </div>
+                </div>
               </li>
             ))}
           </ul>
