9590bbb273b51589a26326b43efb7581dcc8bb70
annieha04 <itsannha@gmail.com>
2024-05-15 18:38:22 -0400

    undo and put original code back

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const SubNavHome = () => {
   return (
